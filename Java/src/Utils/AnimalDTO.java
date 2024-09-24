2dfda6aa4c0249f30a44aeb7b018a01e90728177
daeldev <danielalmeidahulk@gmail.com>
2024-09-24 11:48:43 -0300

    refactor: AnimalDTO

---
+++ Java/src/Utils/AnimalDTO.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Utils;
+
+/**
+ *
+ * @author Oliveira
+ */
+public class AnimalDTO {
+    // idAnimal
+    private int idAnimal;
+    private String nome;
+    private String generoAnimal;
+    private String especie;
+    private String raca;
+    private String idade;
+    private String descricao;
+    private String cor;
+    private double peso;
+    private byte[] fotoAnimal;
+    private int cliente;
+
+    public int getIdAnimal() {
+        return idAnimal;
+    }
+
+    public void setIdAnimal(int idAnimal) {
+        this.idAnimal = idAnimal;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public byte[] getFotoAnimal() {
+        return fotoAnimal;
+    }
+
+    public void setFotoAnimal(byte[] fotoAnimal) {
+        this.fotoAnimal = fotoAnimal;
+    }
+
+    public int getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(int cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getGeneroAnimal() {
+        return generoAnimal;
+    }
+
+    public void setGeneroAnimal(String generoAnimal) {
+        this.generoAnimal = generoAnimal;
+    }
+}
